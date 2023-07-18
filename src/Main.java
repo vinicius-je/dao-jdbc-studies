8b8370da62c13f145ae05110fbbed33dd7c40c6f
vinicius-je <vinicius.je99@gmail.com>
2023-07-18 11:20:22 -0300

    feat: add seller findById

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }
