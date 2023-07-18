9e8e9227b1be98c930d887dadf51942211f3e7eb
vinicius-je <vinicius.je99@gmail.com>
2023-07-18 11:19:50 -0300

    feat: add department in seller entitie class

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -11,14 +11,17 @@ public class Seller implements Serializable {
     private Date birthDate;
     private Double baseSalary;
 
+    private Department department;
+
     public Seller(){}
 
-    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary) {
+    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.birthDate = birthDate;
         this.baseSalary = baseSalary;
+        this.department = department;
     }
 
     public Integer getId() {
@@ -61,6 +64,14 @@ public class Seller implements Serializable {
         this.baseSalary = baseSalary;
     }
 
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -82,6 +93,7 @@ public class Seller implements Serializable {
                 ", email='" + email + '\'' +
                 ", birthDate=" + birthDate +
                 ", baseSalary=" + baseSalary +
+                ", department=" + department +
                 '}';
     }
 }
